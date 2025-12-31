3ebad50f98c21a7cca27d6b1c4f8bacf1c26183e
Chris Thoburn <runspired@users.noreply.github.com>
2025-12-31 13:50:25 -0800

    remove extra base

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,7 +3,6 @@ import { extensions, ember } from '@embroider/vite';
 import { babel } from '@rollup/plugin-babel';
 
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/bandits.run/' : '/',
   plugins: [
     ember(),
     // extra plugins here
