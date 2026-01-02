3403c9a42cf173ccd1d90857b14d38213a4c8bc1
Chris Thoburn <runspired@users.noreply.github.com>
2026-01-02 01:07:28 -0800

    add 5th Saturday

---
+++ api/seeds/runs/bay-bandits/the-saturday-dealers-choice.ts
@@ -0,0 +1,58 @@
+import type { TrailRun } from "../../../interfaces/run";
+
+export const data = {
+  "title": "5th Saturday | Dealer's Choice",
+  "hosts": ['1'],
+  "organizers": ['1', '7'],
+  "eventLink": "",
+  "runs": [
+    {
+      "name": "8ish",
+      "leaders": [],
+      "distance": "~8 Mi",
+      "vert": "1200-2000ft",
+      "pace": "Easy",
+      "category": "at-your-own-pace",
+      "meetTime": "08:15",
+      "startTime": "08:30",
+      "eventLink": null,
+      "stravaRouteLink": null,
+      "gpxLink": null
+    },
+    {
+      "name": "12ish",
+      "leaders": [],
+      "distance": "~12 Mi",
+      "vert": "1800-3000ft",
+      "pace": "Easy",
+      "category": "at-your-own-pace",
+      "meetTime": "08:15",
+      "startTime": "08:30",
+      "eventLink": null,
+      "stravaRouteLink": null,
+      "gpxLink": null
+    },
+    {
+      "name": "16ish",
+      "leaders": [],
+      "distance": "~16 Mi",
+      "vert": "2700-4000ft",
+      "pace": "Easy",
+      "category": "at-your-own-pace",
+      "meetTime": "08:15",
+      "startTime": "08:30",
+      "eventLink": null,
+      "stravaRouteLink": null,
+      "gpxLink": null
+    }
+  ],
+  "location": "",
+  "description": "",
+  "recurrence": {
+    "day": 6,
+    "frequency": "monthly",
+    "interval": 1,
+    "weekNumber": 5,
+    "date": null
+  }
+} satisfies TrailRun;
