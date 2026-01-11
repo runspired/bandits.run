cfa6f1f73e9f7938af2d508f6f9ad8fda8d6f2e8
Chris Thoburn <runspired@users.noreply.github.com>
2026-01-11 13:25:40 -0800

    fix data

--- api/seeds/organizations/006_marin-running-club.ts
+++ api/seeds/organizations/006_marin-running-club.ts
@@ -1,11 +1,11 @@
 import type { Organization} from "../../interfaces/organization";
 
 export const data = {
-  name: "Marin Run Club",
+  name: "Marin Running Club",
   contacts: ["9"],
   website: "https://marinrunningclub.com",
   stravaId: "191891",
-  stravaHandle: "marin-run-club",
+  stravaHandle: "m-r-c",
   meetupId: "marin-running-club",
   instagramHandle: null,
   email: null,
